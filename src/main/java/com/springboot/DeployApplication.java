b43e955d7ee76a894aabe42cbd627d34c6c3aedf
ashraf <alamashraf356@gmail.com>
2022-04-22 22:23:43 +0530

    First Commit

---
+++ src/main/java/com/springboot/DeployApplication.java
@@ -0,0 +1,21 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DeployApplication {
+	
+	@GetMapping("/home")
+	public String Home() {
+		return "This is home Page of springboot application";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployApplication.class, args);
+	}
+
+}
